aff94203afea2fbd21ab223829d70943dba4a870
Yegor Bugayenko <yegor256@gmail.com>
2022-09-18 18:35:14 +0300

    #7 no announcement

--- build.lua
+++ build.lua
@@ -8,7 +8,6 @@ uploadconfig = {
   uploader = "Yegor Bugayenko",
   email = "yegor256@gmail.com",
   note = "Bug fixes",
-  announcement = "Some non-critical bug fixes",
   ctanPath = "/macros/latex/contrib/ppt-slides",
   bugtracker = "https://github.com/yegor256/ppt-slides/issues",
   description = "This LaTeX package helps you create slide decks as good-looking as with PowerPoint™, but more precise, uniform, and visually strict. Check <a href='https://github.com/yegor256/ssd16'>this series of lectures</a> fully designed with the use of this package.",
