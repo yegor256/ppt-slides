a8c11e00c1581270582e27759444196fbd696c0d
Yegor Bugayenko <yegor256@gmail.com>
2022-12-03 08:27:23 +0300

    #10 copy

--- build.lua
+++ build.lua
@@ -4,7 +4,7 @@ typesetopts = "-interaction=batchmode -shell-escape -halt-on-error"
 checkopts = "-interaction=batchmode -shell-escape -halt-on-error"
 checkengines = {"pdftex"}
 tagfiles = {"build.lua", "ppt-slides.dtx"}
-docfiles = {"socrates.jpg"}
+docfiles = {"socrates.jpg", "ppt-templates", "ppt-schemes"}
 cleanfiles = {"build", "_docshots", "*.run.xml", "*.log", "*.crumbs", "*.bcf", "*.glo", "*.fls", "*.idx", "*.out", "*.fdb_latexmk", "*.aux", "*.sty", "*.zip", "ppt-slides.pdf"}
 typesetruns = 2
 
